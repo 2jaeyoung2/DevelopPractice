473502a9d0e46de8246cd8ae4600eb83778db2c7
2jaeyoung2 <i0579923@gmail.com>
2026-03-15 17:09:46 +0900

    [Temp]moving in progress

--- Assets/01. Scripts/PlayerMovement.cs
+++ Assets/01. Scripts/PlayerMovement.cs
@@ -16,23 +16,63 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 moveDir;
 
-    private void OnMove(InputAction.CallbackContext ctx)
+    private bool isGrounded;
+
+    private bool isMoving = false;
+
+    private bool isJumpPressed;
+
+    public void OnMove(InputAction.CallbackContext ctx)
     {
         if (ctx.phase == InputActionPhase.Performed)
         {
+            isMoving = true;
+
             moveDir = ctx.ReadValue<Vector2>();
         }
         else
         {
+            isMoving = false;
+
             moveDir = Vector2.zero;
+
+            rb.velocity = Vector3.zero;
         }
     }
 
-    private void Update()
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.phase == InputActionPhase.Performed)
+        {
+            // TODO: มกวม
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Jump()
+    {
+
+    }
+
+    private void Move()
     {
         if (moveDir != Vector2.zero)
         {
+            moveSpeed = 0.1f;
 
+            rotateSpeed = 10f;
+
+            Vector3 move = moveSpeed * new Vector3(moveDir.x, 0, moveDir.y);
+
+            rb.MovePosition(transform.position + move);
+
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotateSpeed);
         }
     }
 }
