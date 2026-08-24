1e23f36c36f9bc7d82f1da59d5dc7f51559db445
2jaeyoung2 <i0579923@gmail.com>
2026-08-24 17:25:53 +0900

    [Temp]Version migration

---
+++ Assets/01. Scripts/Map Generator/TileType.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileType", menuName = "ScriptableObject/TileData")]
+public class TileType : ScriptableObject
+{
+    public CubeType cubeType;
+}
